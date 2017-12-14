1a1124775181a62effe1ac52ada2d39282d82366
burglarhobbit <bhavyapatwa007@gmail.com>
2017-12-14 22:42:40 +0530

    Updated Code

--- get_inputs.py
+++ get_inputs.py
@@ -1,4 +1,13 @@
 import json
+import numpy as np
+import sys
+import re
+from keras.preprocessing.text import Tokenizer
+
+if (sys.version_info > (3, 0)):
+	import _pickle as pickle
+else:
+	import cPickle as pickle
 
 # training
 file_t = 'train_v1.1.json'
@@ -6,15 +15,30 @@ file_t = 'train_v1.1.json'
 # sample
 file_s = 'sample.json'
 
+GLOVE_PATH = "./glove.6B/glove.6B.300d.txt"
 
-def get_all(file_n=file_s):
+def tokenize(data):
+	return [x.strip().lower() for i in re.split('(\W+)?', data) if i.strip()]
+			
+def get_all_qpa_tokens(file_n=file_s):
 
+	question = ''
+	passage = []
+	answer = ''
 	with open(file_n) as f:
 		line = f.readline()
-		print line
+		#print line
 		dic = json.loads(line)
+		j = []
+		question = dic['query']
+		answer = dic['answers'][0]
 		for i in dic.keys():
-			print i
+			#print dic[i]
+			j += [i]
+		for i in dic['passages']:
+			#passage.append(i['passage_text'].encode('utf-8').strip())
+			passage_text = i['passage_text']
+			passage.append(i['passage_text'])
 			"""
 			if type(dic[i]) == type({}):
 				for j in dic[i].keys():
@@ -25,11 +49,16 @@ def get_all(file_n=file_s):
 			else:
 				print dic[i]
 				#print dic[i]
+			import get_inputs as g
+			g.get_all()
 			"""
+		question = tokenize(question)
+		passage = [tokenize(i) for i in passage]
+		answer = tokenize(answer)
+	return question_t,passage_t,answer_t
 
-def save_glove_300d_dic_numpy(path="./glove.6B/glove.6B.300d.txt"):
-	import numpy as np
-	import cPickle as pickle
+def save_glove_300d_dic_numpy(path=GLOVE_PATH):
+	import os
 	embeddings_index = {}
 	#embeddings_vector = np.array()
 	f = open(path)
@@ -38,12 +67,20 @@ def save_glove_300d_dic_numpy(path="./glove.6B/glove.6B.300d.txt"):
 		word = values[0]
 		coefs = np.asarray(values[1:], dtype='float32')
 		embeddings_index[word] = coefs
-	with open('glove.6B.300d.pickle','wb') as handle:
+	with open('./glove.6B/glove.6B.300d.pickle','wb') as handle:
 		#pickle.dump(embeddings_index, handle, protocol=pickle.HIGHEST_PROTOCOl)
 		pickle.dump(embeddings_index, handle)
 
+def load_embeddings():
+	with open('./glove.6B/glove.6B.300d.pickle','rb') as handle:
+		#pickle.dump(embeddings_index, handle, protocol=pickle.HIGHEST_PROTOCOl)
+		embeddings = pickle.load(handle)
+		return embedding
+
 def get_all_embeddings():
-	pass
+	question_t,passage_t,answer_t = get_all_qpa_tokens()
+	embedding = load_embeddings()
+	
 
 def get_all_questions():
 	pass
@@ -54,5 +91,5 @@ def get_all_passages():
 def get_all_answers():
 	pass
 
-def get_embeddings():
-	pass
+def get_embeddings(data):
+	embedding = load_embeddings()
